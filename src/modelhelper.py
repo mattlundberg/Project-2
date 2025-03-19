d8cbc72a17606eab71d41b7b10f6ac641b1b2a08
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 <matthew.d.lundberg@gmail.com>
2025-03-18 20:05:51 -0600

    adding folder for matt

--- src/modelhelper.py
+++ src/modelhelper.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
